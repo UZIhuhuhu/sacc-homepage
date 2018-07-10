1adb1581d64e281f782937dd5cc67d0718a97368
UZIhuhuhu <1908341144@qq.com>
2018-07-10 12:49:58 +0800

    add contact qrcode

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ var app = new Vue({
   el: "#root",
   data: {
     slogan: `SACC`,
-    navigationList: [`首页`, `部门介绍`, `活动通知`, `科协人`],
+    navigationList: [`首页`, `部门介绍`, `活动通知`, `科协人`, `联系我们`],
     departMent: [
       {
         name: `前端组`,
-        detail: ` 这里是内容(前端开发`
+        detail: ` 千变万化--大前端`
       },
       {
         name: `后端组`,
-        detail: ` 这里是内容(前端开发m`
+        detail: `分布式--后台`
       },
       {
         name: `安全组`,
-        detail: ` 这里是内容(前端开发m`
+        detail: `hacker集中地`
       },
       {
         name: `嵌入式开发组`,
@@ -47,7 +47,11 @@ var app = new Vue({
     //到顶部的距离
     topDistance: 0,
     //导航栏的阴影
-    navigationShadow: false
+    navigationShadow: false,
+
+    //qrcode
+    qrcodeStatus: false,
+    qrcodeStatus2: false
   },
 
   methods: {
@@ -65,31 +69,73 @@ var app = new Vue({
       } else {
         this.navigationShadow = false;
       }
-      console.log(this.topDistance);
+      // console.log(this.topDistance);
     },
     //导航栏 => 改变到顶部的距离
     //100vh == 976px
     changeDistance: function(e) {
       let text = e.toElement.innerText;
       if (text === `首页`) {
-        document.documentElement.scrollTop = 0;
+        this.calculateDistance(0);
       } else if (text === `部门介绍`) {
-        document.documentElement.scrollTop = 976;
+        this.calculateDistance(976);
       } else if (text === `活动通知`) {
-        document.documentElement.scrollTop = 976 * 2;
+        this.calculateDistance(976 * 2);
       } else if (text === `科协人`) {
-        document.documentElement.scrollTop = 976 * 3;
+        this.calculateDistance(976 * 3);
+      } else if (text === `联系我们`) {
+        this.calculateDistance(976 * 3.5);
       }
     },
 
     //距离计算
     calculateDistance: function(targetDistance = 0) {
       if (targetDistance <= this.topDistance) {
+        let reduce = setInterval(() => {
+          console.log(document.documentElement.scrollTop);
+          document.documentElement.scrollTop -= 10;
+          if (
+            Math.abs(
+              Number(document.documentElement.scrollTop) -
+                Number(targetDistance)
+            ) <= 10
+          ) {
+            document.documentElement.scrollTop = targetDistance;
+            clearInterval(reduce);
+          }
+        }, 10);
       } else {
+        let add = setInterval(() => {
+          document.documentElement.scrollTop += 10;
+          if (
+            Math.abs(
+              Number(document.documentElement.scrollTop) -
+                Number(targetDistance)
+            ) <= 10
+          ) {
+            document.documentElement.scrollTop = targetDistance;
+            clearInterval(add);
+          }
+        }, 10);
       }
+    },
+    //二维码的动画
+    qrcodeDisplay: function() {
+      this.qrcodeStatus = true;
+    },
+    qrcodeDisappear: function() {
+      this.qrcodeStatus = false;
+    },
+    qrcodeDisplay2: function() {
+      this.qrcodeStatus2 = true;
+    },
+    qrcodeDisappear2: function() {
+      this.qrcodeStatus2 = false;
     }
   },
+
   mounted() {
     window.addEventListener("scroll", this.handleScroll);
+    // window.addEventListener("mouseover",this.qrcodeDisplay);
   }
 });
