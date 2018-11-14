9753940f1d215dde018e7287323c0e21762ed7d8
UZIhuhuhu <1908341144@qq.com>
2018-11-14 22:52:50 +0800

    style: remove reduntant sass code

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: yellowDog 
+ * @Date: 2018-10-11 10:33:50 
+ * @Last Modified by:   yellowDog 
+ * @Last Modified time: 2018-10-11 10:33:50 
+ */
 import Vue from "vue";
 import App from "./App.vue";
 import "./plugin/element";
